97a737b1f04b16cfd2a77651bea0da8bc6658834
markup54 <marzenakupka@tlen.pl>
2023-10-20 09:53:45 +0200

    metody cyklu zycia aktywnosci

--- app/src/main/java/pl/zabrze/zs10/czas_zycia_3p_piatek/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/czas_zycia_3p_piatek/MainActivity.java
@@ -1,8 +1,10 @@
 package pl.zabrze.zs10.czas_zycia_3p_piatek;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,9 +17,54 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("CZAS_ZYCIA","Uruchomiana metoda onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("CZAS_ZYCIA","Uruchomiana metoda onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("CZAS_ZYCIA","Uruchomiana metoda onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("CZAS_ZYCIA","Uruchomiana metoda onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("CZAS_ZYCIA","Uruchomiana metoda onPause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("CZAS_ZYCIA","Uruchomiana metoda onStop");
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i("CZAS_ZYCIA","Uruchomiona metoda onSaveInstanceState");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("CZAS_ZYCIA","Uruchomiana metoda onCreate");
         buttonGora = findViewById(R.id.imageButton);
         buttonDol = findViewById(R.id.imageButton2);
         textView = findViewById(R.id.textView);
